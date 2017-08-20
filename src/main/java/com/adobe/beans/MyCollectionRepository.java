93bafbef6059919210b75c1b279e298c1e4bb595
Varun <eshanna@adobe.com>
2017-08-20 20:29:15 +0530

    Deleting unwanted files

--- src/main/java/com/adobe/beans/MyCollectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.adobe.beans;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyCollectionRepository extends MongoRepository<MyCustomCollection, String> {
-
-}
